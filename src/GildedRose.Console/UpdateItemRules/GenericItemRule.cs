5f5453b8daa2f49bc234cc59e944412d3fdff724
Robert Fensterman <minnmass@gmail.com>
2022-10-04 22:32:40 -0500

    framework for extracting quality update rules

---
+++ src/GildedRose.Console/UpdateItemRules/GenericItemRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Console.UpdateItemRules {
+	internal class GenericItemRule : IUpdateItemRule {
+		private static int MinQuality = 0;
+
+		public void Update(Item item) {
+			throw new Exception("boom - not ready for use in refactoring");
+			int newQuality = item.Quality - (item.SellIn > 0 ? 1 : 2);
+			item.Quality = Math.Max(MinQuality, newQuality);
+
+			--item.SellIn;
+		}
+	}
+}
