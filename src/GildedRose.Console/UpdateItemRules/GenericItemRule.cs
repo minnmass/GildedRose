d1a5f8e98c60e9ae7d46e5c724efa6e05956716f
Robert Fensterman <minnmass@gmail.com>
2022-10-05 18:28:53 -0500

    add support for conjured items, assuming that conjured items start with "conjured" (and that "starts with" is a reasonable rule for identifying an item's class)

--- src/GildedRose.Console/UpdateItemRules/GenericItemRule.cs
+++ src/GildedRose.Console/UpdateItemRules/GenericItemRule.cs
@@ -2,10 +2,12 @@
 
 namespace GildedRose.Console.UpdateItemRules {
 	internal class GenericItemRule : IUpdateItemRule {
-		public void Update(Item item) {
+		public void UpdateQuality(Item item) {
 			int newQuality = item.Quality - (item.SellIn > 0 ? 1 : 2);
 			item.Quality = Math.Max(ItemRuleDefaults.MinQuality, newQuality);
+		}
 
+		public void UpdateSellIn(Item item) {
 			--item.SellIn;
 		}
 	}
---
+++ src/GildedRose.Console/UpdateItemRules/QualityModificationDoublingRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GildedRose.Console.UpdateItemRules {
+	internal class QualityModificationDoublingRule : IUpdateItemRule {
+		private readonly IUpdateItemRule _baseRule;
+
+		public QualityModificationDoublingRule(IUpdateItemRule baseRule) {
+			_baseRule = baseRule ?? throw new ArgumentNullException(nameof(baseRule));
+		}
+
+		public void UpdateQuality(Item item) {
+			_baseRule.UpdateQuality(item);
+			_baseRule.UpdateQuality(item);
+		}
+
+		public void UpdateSellIn(Item item) {
+			_baseRule.UpdateSellIn(item);
+		}
+	}
+}
